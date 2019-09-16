2064ac0b5315560a2a0d92abe43022cebaa53391
tccontre <26181693+tccontre@users.noreply.github.com>
2019-09-16 14:31:42 +0200

    Update AboutEchoEditor.Designer.cs

--- EchoEditor/EchoEditor/AboutEchoEditor.Designer.cs
+++ EchoEditor/EchoEditor/AboutEchoEditor.Designer.cs
@@ -1,4 +1,24 @@
-﻿namespace EchoEditor
+/*
+# Copyright 2019 - tccontre
+
+# This file is part of EchoEditor.
+
+# EchoEditor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# EchoEditor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with EchoEditor.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+namespace EchoEditor
 {
     partial class AboutEchoEditor
     {
